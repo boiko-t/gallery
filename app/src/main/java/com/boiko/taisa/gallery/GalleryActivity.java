f299155a4524ec767b596314e262635582eebffc
Taisa Boiko <tayaboyko@gmail.com>
2018-05-03 10:30:16 +0300

    update(GalleryActivity): gson test

--- app/src/main/java/com/boiko/taisa/gallery/GalleryActivity.java
+++ app/src/main/java/com/boiko/taisa/gallery/GalleryActivity.java
@@ -1,7 +1,13 @@
 package com.boiko.taisa.gallery;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.boiko.taisa.gallery.Models.GalleryItem;
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class GalleryActivity extends AppCompatActivity {
 
@@ -9,5 +15,14 @@ public class GalleryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
+        InputStreamReader stream;
+        try {
+            stream = new InputStreamReader(getAssets().open("galleryItems.json"));
+        }catch (IOException e){
+            e.printStackTrace();
+            return;
+        }
+        Gson gson = new Gson();
+        GalleryItem item = gson.fromJson(stream, GalleryItem.class);
     }
 }
